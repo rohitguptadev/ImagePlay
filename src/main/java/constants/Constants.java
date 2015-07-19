c22ebc5d47e4d8484b4e29a95659096d3c8dd02b
Rohit Gupta <rohit221991@iitj.ac.in>
2015-07-19 17:25:42 +0530

    secret keys of Imagga removed

--- src/main/java/constants/Constants.java
+++ src/main/java/constants/Constants.java
@@ -1,8 +1,9 @@
 package constants;
 
 public class Constants {
-	
-	public static final String FLICKER_KEY="YOUR_FLICKER_API_KEY";
-	public static final String FLICKER_SECRET="YOUR_FLICKER_SECRET";
+
+	public static final String FLICKER_KEY = "YOUR_FLICKER_API_KEY";
+	public static final String FLICKER_SECRET = "YOUR_FLICKER_SECRET";
+	public static final String IMAGGA_KEY_SECRET_BASE64 = "YOUR_FLICKER_SECRET";
 
 }
